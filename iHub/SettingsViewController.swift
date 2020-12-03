0a1d57ed2e7518307d574110fbf25ad8614415ab
OldWorldForest <oldworldforest@gmail.com>
2020-12-02 21:46:14 -0800

    Added Settings VC and Swift, All Swift file, linked Settings

---
+++ iHub/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  iHub
+//
+//  Created by Aldo Flores on 12/2/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
