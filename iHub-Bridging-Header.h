ca0dedd5824622a3198c2a016b9e5c0d401a2ddf
OldWorldForest <oldworldforest@gmail.com>
2020-12-09 22:33:11 -0800

    bridge header

---
+++ iHub-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <TMTumblrSDK/TMOAuthAuthenticator.h>
+#import <TMTumblrSDK/TMURLSession.h>
+#import "TMOAuthAuthenticatorDelegate.h"
+#import "TMBasicBaseURLDeterminer.h"
+#import <TMTumblrSDK/TMHTTPRequest.h>
+#import <TMTumblrSDK/TMURLSession.h>
+#import <TMTumblrSDK/TMOAuth.h>
---
+++ tmblr.h
@@ -0,0 +1,17 @@
+//
+//  tmblr.h
+//  iHub
+//
+//  Created by Aldo Flores on 12/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface tmblr : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
